b399cc263324f9d7b51a2627198a9171f4162196
andrewsource147 <31321699+andrewsource147@users.noreply.github.com>
2023-05-05 11:37:57 +0700

    CLI for smartwallet (#2)
    * CLI for smartwallet
    
    * remove unused files

---
+++ cli/smartwallet/src/args.rs
@@ -0,0 +1,107 @@
+use anchor_client::solana_sdk::pubkey::Pubkey;
+use anchor_client::Cluster;
+use clap::*;
+
+#[derive(Parser, Debug)]
+pub struct ConfigOverride {
+    /// Cluster override
+    ///
+    /// Values = Mainnet, Testnet, Devnet, Localnet.
+    /// Default: Devnet
+    #[clap(global = true, short, long, default_value_t = Cluster::Devnet)]
+    pub cluster: Cluster,
+    /// Wallet override
+    ///
+    /// Example: /path/to/wallet/keypair.json
+    /// Default: ~/.config/solana/id.json
+    #[clap(
+        global = true,
+        short,
+        long,
+        default_value_t = String::from(shellexpand::tilde("~/.config/solana/id.json"))
+    )]
+    pub wallet_path: String,
+
+    /// Base keypair file required to initialize the vault
+    ///
+    /// /path/to/base/keypair.json
+    // #[clap(global = true, short, long, default_value_t = String::from(""))]
+    // pub base: String,
+
+    #[clap(
+    global = true,
+    short,
+    long,
+    default_value_t = smart_wallet::id().to_string()
+    )]
+    pub program_id: String,
+}
+
+#[derive(Parser, Debug)]
+pub enum CliCommand {
+    CreateSmartWallet {
+        #[clap(long)]
+        max_owners: u8,
+        #[clap(long)]
+        threshold: u64,
+        #[clap(long)]
+        minimum_delay: i64,
+        #[clap(long)]
+        owners: Vec<Pubkey>,
+    },
+
+    SetOwners {
+        #[clap(long)]
+        smart_wallet: Pubkey,
+        #[clap(long)]
+        owners: Vec<Pubkey>,
+    },
+
+    ChangeThreshold {
+        #[clap(long)]
+        smart_wallet: Pubkey,
+        #[clap(long)]
+        threshold: u64,
+    },
+
+    AprroveTransaction {
+        #[clap(long)]
+        smart_wallet: Pubkey,
+        #[clap(long)]
+        transaction: Pubkey,
+    },
+    UnAprroveTransaction {
+        #[clap(long)]
+        smart_wallet: Pubkey,
+        #[clap(long)]
+        transaction: Pubkey,
+    },
+    ExecuteTransaction {
+        #[clap(long)]
+        smart_wallet: Pubkey,
+        #[clap(long)]
+        transaction: Pubkey,
+    },
+    ViewSmartwallet {
+        #[clap(long)]
+        smart_wallet: Pubkey,
+    },
+    ViewTransaction {
+        #[clap(long)]
+        transaction: Pubkey,
+    },
+    // only for test
+    CreateDummyTransaction {
+        #[clap(long)]
+        smart_wallet: Pubkey,
+    },
+}
+
+#[derive(Parser, Debug)]
+#[clap(version, about, author)]
+pub struct Opts {
+    #[clap(flatten)]
+    pub config_override: ConfigOverride,
+    #[clap(subcommand)]
+    pub command: CliCommand,
+}
