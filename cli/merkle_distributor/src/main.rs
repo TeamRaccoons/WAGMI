bd54c4911a83956d0dbd8695f4d0ef68200a8fe8
Andrew Nguyen <cuong.hv.dtvt@gmail.com>
2023-05-10 09:42:02 +0700

    fix build tree

--- cli/merkle_distributor/src/main.rs
+++ cli/merkle_distributor/src/main.rs
@@ -35,21 +35,28 @@ fn main() -> Result<()> {
         CommitmentConfig::finalized(),
     );
 
+    let base = match opts.config_override.base {
+        Some(value) => {
+            read_keypair_file(&*shellexpand::tilde(&value)).expect("Requires a keypair file")
+        }
+        None => Keypair::new(),
+    };
     let program = client.program(program_id);
     match opts.command {
         CliCommand::NewDistributor {
-            locker,
             token_mint,
             path_to_snapshot,
         } => {
-            new_distributor(&program, locker, token_mint, path_to_snapshot)?;
+            new_distributor(&program, base, token_mint, path_to_snapshot)?;
+        }
+        CliCommand::Fund { path_to_snapshot } => {
+            fund(&program, base, path_to_snapshot)?;
         }
         CliCommand::Claim {
-            distributor,
             claimant,
             path_to_snapshot,
         } => {
-            claim(&program, distributor, claimant, path_to_snapshot)?;
+            claim(&program, base, claimant, path_to_snapshot)?;
         }
         CliCommand::ViewDistributor { distributor } => {
             let distributor_state: merkle_distributor::MerkleDistributor =
@@ -82,15 +89,17 @@ fn main() -> Result<()> {
 
 fn new_distributor(
     program: &Program,
-    locker: Pubkey,
+    base_keypair: Keypair,
     token_mint: Pubkey,
     path_to_snapshot: String,
 ) -> Result<()> {
     let snapshot = read_snapshot(path_to_snapshot);
     let (max_num_nodes, max_total_claim, root) = build_tree(&snapshot);
 
-    let base_keypair = Keypair::new();
     let base = base_keypair.pubkey();
+
+    let (locker, _bump) =
+        Pubkey::find_program_address(&[b"Locker".as_ref(), base.as_ref()], &voter::id());
 
     let (distributor, _bump) = Pubkey::find_program_address(
         &[b"MerkleDistributor".as_ref(), base.as_ref()],
@@ -119,16 +128,71 @@ fn new_distributor(
     Ok(())
 }
 
+fn fund(program: &Program, base_keypair: Keypair, path_to_snapshot: String) -> Result<()> {
+    let snapshot = read_snapshot(path_to_snapshot);
+    let (max_num_nodes, max_total_claim, root) = build_tree(&snapshot);
+
+    let base = base_keypair.pubkey();
+
+    let (distributor, _bump) = Pubkey::find_program_address(
+        &[b"MerkleDistributor".as_ref(), base.as_ref()],
+        &merkle_distributor::id(),
+    );
+
+    let distributor_state: merkle_distributor::MerkleDistributor = program.account(distributor)?;
+    let token_mint = distributor_state.mint;
+    println!("{} {}", token_mint, max_total_claim);
+    // let source_pubkey = get_associated_token_address(&program.payer(), &token_mint);
+    let destination_pubkey = get_associated_token_address(&distributor, &token_mint);
+
+    let instructions = vec![
+        spl_token::instruction::mint_to(
+            &spl_token::id(),
+            &token_mint,
+            &destination_pubkey,
+            &program.payer(),
+            &[],
+            max_total_claim,
+        )?,
+        // spl_token::instruction::transfer(
+        //     &spl_token::id(),
+        //     &source_pubkey,
+        //     &destination_pubkey,
+        //     &program.payer(),
+        //     &vec![],
+        //     max_total_claim,
+        // )?,
+    ];
+
+    let builder = program.request();
+    let builder = instructions
+        .into_iter()
+        .fold(builder, |bld, ix| bld.instruction(ix));
+
+    let result = simulate_transaction(&builder, program, &vec![&default_keypair()]).unwrap();
+    println!("{:?}", result);
+    return Ok(());
+
+    let signature = builder.send()?;
+    println!("Signature {:?}", signature);
+    Ok(())
+}
+
 fn claim(
     program: &Program,
-    distributor: Pubkey,
+    base_keypair: Keypair,
     claimant: Pubkey,
     path_to_snapshot: String,
 ) -> Result<()> {
     let snapshot = read_snapshot(path_to_snapshot);
 
     let (index, amount, proof) = snapshot.get_user_claim_info(claimant)?;
+    let base = base_keypair.pubkey();
 
+    let (distributor, _bump) = Pubkey::find_program_address(
+        &[b"MerkleDistributor".as_ref(), base.as_ref()],
+        &merkle_distributor::id(),
+    );
     let distributor_state: merkle_distributor::MerkleDistributor = program.account(distributor)?;
 
     let (claim_status, _bump) = Pubkey::find_program_address(
